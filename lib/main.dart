05d7004678aaa84be8939157276af46cbc7bfdae
Thorn Chorn <chornthorn.kh@gmail.com>
2025-03-21 20:26:21 +0700

    Add firebase web

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,36 @@
+import 'package:device_preview_plus/device_preview_plus.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import 'data/services/auth_service.dart';
 import 'routing/router_config.dart';
+import 'ui/core/themes/app_colors.dart';
 import 'ui/core/themes/app_theme.dart';
 
 void main() {
   // Ensure Flutter bindings are initialized
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const KhodeExpenseApp());
+  runApp(
+    // Only use DevicePreview in web
+    kIsWeb
+        ? DevicePreview(
+          enabled: true,
+          isToolbarVisible: false,
+          backgroundColor: AppColors.cardBackground,
+          defaultDevice: DeviceInfo.genericPhone(
+            platform: TargetPlatform.iOS,
+            id: 'iphone-13-pro-max',
+            name: 'iPhone 13 Pro Max',
+            screenSize: Size(428, 926),
+            safeAreas: EdgeInsets.zero,
+            pixelRatio: 3.0,
+            framePainter: GenericPhoneFramePainter(),
+          ),
+          builder: (context) => const KhodeExpenseApp(),
+        )
+        : const KhodeExpenseApp(),
+  );
 }
 
 class KhodeExpenseApp extends StatelessWidget {
